b9d11c24062484ec3291747c75e7143912ee2304
Olivier Youmbi <110872420+MES-DASH-B@users.noreply.github.com>
2025-03-16 17:35:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown("##")
 
 
 #___________________________
-@st.cache_data
+@st.cache_data(ttl=0)
 def load_css(file_name):
     with open(file_name, "r") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
