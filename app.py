91c09186256cbce901bad0c63fbed25e0ceaa2d4
Olivier Youmbi <110872420+MES-DASH-B@users.noreply.github.com>
2025-03-19 13:34:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -215,26 +215,139 @@ if selected == "ANALYS MAINT.":
         usecols='B:H',
         nrows=150,
         )
+    
+    df4= pd.read_excel(
+        io='CALCUL_TRS.xlsx',
+        engine='openpyxl',
+        sheet_name='DétailTRS',
+        skiprows=4,
+        usecols='B:AE',
+        nrows=150,
+        )
+    
+    
     #st.dataframe(df2)
     
     Semaine = st.sidebar.multiselect(
         "Choisir la semaine:",
-        options = df2["Semaine"].unique(),
-        default = df2["Semaine"].unique())
+        options = df4["Semaine"].unique(),
+        default = df4["Semaine"].unique())
     
-    df2_selection = df2.query("Semaine == @Semaine")
+    df4_selection = df4.query("Semaine == @Semaine")
     
         # Get unique values from "Semaine" column
-    semaines = df2["Semaine"].unique()
+    semaines = df4["Semaine"].unique()
 
     # Sidebar filter
     selected_semaine = st.sidebar.selectbox("Select a Week:", semaines)
 
     # Filter the DataFrame based on the selected week
-    df_filtered = df2[df2["Semaine"] == selected_semaine]
+    df_filtered = df4[df4["Semaine"] == selected_semaine]
 
     # Display the filtered data
     #st.write(df_filtered)
+
+
+    Objectifs1= int(df_filtered["Objectif1"].sum())
+    Qté1= int(df_filtered["Qté produite1"].sum())
+    Écart1= int(df_filtered["Écart1"].sum())
+    trs1= int((df_filtered["TRS1"].sum())*100)
+
+    st.subheader("MS20")
+    col11, col21, col31, col41 = st.columns(4)
+
+    with col11:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Objectifs1}</p></div>""",unsafe_allow_html=True)
+    with col21:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Qté1}</p></div>""",unsafe_allow_html=True)
+    with col31:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Écart1}</p></div>""",unsafe_allow_html=True)
+    with col41:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{trs1} %</p></div>""",unsafe_allow_html=True)
+
+
+    Objectifs2= int(df_filtered["Objectif2"].sum())
+    Qté2= int(df_filtered["Qté produite2"].sum())
+    Écart2= int(df_filtered["Écart2"].sum())
+    trs2= int((df_filtered["TRS2"].sum())*100)
+
+    st.subheader("SHINKO 1 (V831)")
+    col110, col210, col310, col410 = st.columns(4)
+
+    with col110:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Objectifs2}</p></div>""",unsafe_allow_html=True)
+    with col210:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Qté2}</p></div>""",unsafe_allow_html=True)
+    with col310:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Écart2}</p></div>""",unsafe_allow_html=True)
+    with col410:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{trs2} %</p></div>""",unsafe_allow_html=True)
+        
+
+    Objectifs3= int(df_filtered["Objectif3"].sum())
+    Qté3= int(df_filtered["Qté produite3"].sum())
+    Écart3= int(df_filtered["Écart3"].sum())
+    trs3= int((df_filtered["TRS3"].sum())*100)
+    
+    st.subheader("SHINKO 2 (V832)")
+    col111, col211, col311, col411 = st.columns(4)
+    
+    with col111:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Objectifs3}</p></div>""",unsafe_allow_html=True)
+    with col211:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Qté3}</p></div>""",unsafe_allow_html=True)
+    with col311:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Écart3}</p></div>""",unsafe_allow_html=True)
+    with col411:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{trs3} %</p></div>""",unsafe_allow_html=True)
+
+
+    Objectifs4= int(df_filtered["Objectif4"].sum())
+    Qté4= int(df_filtered["Qté produite4"].sum())
+    Écart4= int(df_filtered["Écart4"].sum())
+    trs4= int((df_filtered["TRS4"].sum())*100)
+    
+    st.subheader("SHINKO 3 (V833)")
+    col112, col212, col312, col412 = st.columns(4)
+    
+    with col112:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Objectifs4}</p></div>""",unsafe_allow_html=True)
+    with col212:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Qté4}</p></div>""",unsafe_allow_html=True)
+    with col312:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{Écart4}</p></div>""",unsafe_allow_html=True)
+    with col412:
+        st.markdown(f"""<div class="metric-container"><p class="metric-label">Nombre total des arrets</p><p class="metric-value">{trs4} %</p></div>""",unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+    figtrs1=go.Figure()
+    
+    figtrs1.add_trace(go.Scatter(x=df4["Semaine"], y=df4["TRS1"], mode='lines', name='Line chart', line=dict(color='blue',width=2)))
+    
+    figtrs1.add_trace(go.Bar(x=df4["Semaine"], y=df4["Qté produite1"], name='Histogramt', marker=dict(color='orange'), yaxis='y2'))
+    
+    figtrs1.update_layout(title="line chart with scaled histogram", xaxis_title="Semainer", yaxis=dict(title="Line chart values", side="left"), yaxis2=dict(title="histogram values(large scale)", overlaying="y", side="right",showgrid=False),barmode='overlay')
+    #figtrs1.show()
+    st.write(figtrs1)
+
+
+
+
+
+
+
+
+
 
     df3["TRS 1"] = (df3["TRS 1"] * 100).round(0).astype(int)
     figTRS = px.line(df3, x="Date",y=df3["TRS 1"],color="Machine",template = 'plotly' )
@@ -243,15 +356,7 @@ if selected == "ANALYS MAINT.":
     figqté = px.bar(df3, x="Date",y=df3["Quantité E1+E2"],color="Machine", template = 'plotly' )
     st.write(figqté)
     
-    figtrs1=go.Figure()
     
-    figtrs1.add_trace(go.Scatter(x=df3["Date"], y=df3["TRS 1"], mode='lines+markers', name='Line chart', line=dict(color='blue',width=2)))
-    
-    figtrs1.add_trace(go.Bar(x=df3["Date"], y=df3["Quantité E1+E2"], name='Histogramt', marker=dict(color='orange'), yaxis='y2'))
-    
-    figtrs1.update_layout(title="line chart with scaled histogram", xaxis_title="Date", yaxis=dict(title="Line chart values", side="left"), yaxis2=dict(title="histogram values(large scale)", overlaying="y", side="right",showgrid=False),barmode='overlay')
-    #figtrs1.show()
-    st.write(figtrs1)
     
     figS = px.line(df2, x="Semaine",y="TRS",color="Machine ", template = 'plotly' )
     st.write(figS)
