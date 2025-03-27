9d1c87bc82ef3ba91446e858d29049454c0b5ce2
Olivier Youmbi <110872420+MES-DASH-B@users.noreply.github.com>
2025-03-27 15:49:01 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -113,9 +113,14 @@ if selected == "ACCUEIL TRS":
         x=labels,
         y=values,
         text=[f"{v:.1f}%" for v in values],
-        textposition="outside"
+        textposition="outside",
+        connector=dict(line=dict(width=2)),  # Make connectors thicker
+        marker=dict(line=dict(width=3))  # Increase border thickness of bars
+            
     ))
-
+    # Increase text & bar width
+    fig.update_traces(textfont=dict(size=16), width=0.8)  # Larger text & bars
+    
     fig.update_layout(
         title="Pertes TRS MS20 par cause d'arrêt",
         xaxis_title="Causes d'arrêt",
